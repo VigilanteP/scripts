2cdf363c65ce4af12ce2657fae7da6dc5425ba21
VigilanteP <Peter.Bergeron@gmail.com>
2024-03-03 00:23:28 -0500

    Flesh out deploy a bit

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,17 +1,19 @@
 DEPLOYMENT_PASSWORD=changeme
+export DEPLOY_ROOT=~/scripts/deploy
 
-# apk add nfc-tools  #Alpibe
-apt install nfc-common #Ubuntu
+# apk add nfc-tools  #Alpine
+apt install exa bat fzf ripgrep zoxide nfs-common fish #Ubuntu
 
-nfspath='truenas:/mnt/media/deploy'
-localpath='/mnt/deploy'
+
+# Set up NFS mount so we can access deployment scripts
+nfspath='truenas:/mnt/media'
+localpath='/mnt/media'
 # opts='nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800  0 0'
-
 mkdir -p $localpath
 mount $nfspath $localpath
 # echo "$nfspath  $localpath  $opts" >> /etc/fstab
 
-# Creates a sudo user with standard defaults
+# Make a user to access the share and then clone the /home repos
 useradd --uid 1000 --user-group --create-home --groups sudo,media --home-dir /home/peter --password $DEPLOYMENT_PASSWORD peter
 cp /mnt/deploy/deploy_id_rsa /home/peter/.ssh/id_rsa
 cd /home/peter || exit
@@ -19,12 +21,13 @@ cd /home/peter || exit
 eval "$(ssh-agent)" && ssh-add
 sudo -u peter git clone git@github.com:VigilanteP/config --initial-dir ~/.config
 sudo -u peter git clone git@github.com:VigilanteP/scripts
-# Need to update the nvim submodule too but not sure how to do that now
 
-export DEPLOY_ROOT=~/scripts/deploy
-
+# Run all scripts in the installers directory in order by default (alphanum) sort
 for script in "$DEPLOY_ROOT"/installers/*; do 
 	$script
 done
 
-sudo -u peter ln -s home/peter/.config/nvim-custom /home/peter/.config/nvim/lua/custom
+# Put custom nvim into place via symlink
+# We also need to update the nvim submodule too but not sure how to do that now
+mkdir -p /home/peter/.config/nvim/lua/custom
+sudo -u peter ln -s /home/peter/.config/nvim-custom /home/peter/.config/nvim/lua/custom
