1f2916598dc6d051ddf44580c389522e6c124a42
Peter Bergeron <Peter.Bergeron@gmail.com>
2024-04-09 16:34:13 -0400

    Fix fish installer

--- deploy/installers/fish.sh
+++ deploy/installers/fish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget 'https://launchpad.net/~fish-shell/+archive/ubuntu/release-3/+files/fish_3.7.0-1~jammy_amd64.deb'
-dpkg -i fish_3.7.0-1~jammy_amd64.deb
-rm fish_3.7.0-1~jammy_amd64.deb 
+wget 'https://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/amd64/fish_3.7.1-1_amd64.deb'
+dpkg -i fish_3.7.1-1_amd64.deb
+rm fish_3.7.1-1_amd64.deb
