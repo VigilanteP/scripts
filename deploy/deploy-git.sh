7d8c1a6258ee12aaf3d86f5016d87dc6005efd23
Peter Bergeron <Peter.Bergeron@gmail.com>
2024-04-18 08:44:22 +0200

    Improve git script

--- deploy/deploy-git.sh
+++ deploy/deploy-git.sh
@@ -1,15 +1,15 @@
-echo "Creating a new SSH key..."
-ssh-keygen
+# echo "Creating a new SSH key..."
+# ssh-keygen
 
 echo "Adding to agent..."
 eval `ssh-agent`
 ssh-add
 
-echo "Adding key to deployment source..."
+# echo "Adding key to deployment source..."
 ssh-copy-id $1
 
 echo "Adding ssh key to github..."
-ssh $1 "bash -c 'gh ssh-key add <<<\$(echo $(cat ~/.ssh/id_rsa.pub))'"
+# ssh $1 "$(cat ~/.ssh/id_rsa.pub) | gh ssh-key add"
 echo "Waiting a moment for key to propogate..."
 sleep 5
 
@@ -23,7 +23,30 @@ cd ~/.config || exit
 git submodule init
 git submodule update
 
-if ! test -e ~/.config/nvim/lua/custom
+if ! test -d ~/.config/nvim/lua/custom
 then
   ln -sfT ~/.config/nvim-custom ~/.config/nvim/lua/custom
 fi
+
+if test -f ~/.config/ssh/config
+then
+  ln -s ~/.config/ssh/config ~/.ssh/
+end
+
+if test -d ~/.config/search/ignore
+then
+  ignorefiles=$(ls ~/.config/search/ignore/*.ignore)
+  if test -z "$ignorefiles"
+  then
+    echo "No ignore files were found in .config/search/ignore"
+  fi
+
+  if grep -E 'active.ignore' <<<$(echo $ignorefiles)
+  then
+    echo "Found existing active.ignore - Skipping installation"
+  fi
+
+  file=$(grep -o '^[^ ]*\.ignore' <<<$ignorefiles)
+  ln -s ~/.config/search/ignore/$file ~/.config/search/ignore/active.ignore
+end
+
