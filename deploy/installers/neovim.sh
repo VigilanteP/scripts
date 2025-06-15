28753fc20a83bca1956155523653c76c685d2b41
Peter Bergeron <Peter.Bergeron@gmail.com>
2025-06-15 19:35:42 -0400

    Fix file and directory names

--- deploy/installers/neovim.sh
+++ deploy/installers/neovim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -LO https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim-linux-x86_64
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-sudo rm nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim-linux-*
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo rm nvim-linux-x86_64.tar.gz
