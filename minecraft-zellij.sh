83205782463f971347cf56faab9a231dd54d3a29
Your Name <you@example.com>
2025-06-16 16:05:11 -0400

    Zellij updates

--- minecraft-zellij.sh
+++ minecraft-zellij.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-zellij delete-session minecraft-session 2>/dev/null
-zellij attach -b minecraft-session && zellij run -- bash /home/peter/minecraft.sh
+zellij delete-session -f minecraft 2>/dev/null
+zellij attach --create-background minecraft
+zellij run -- bash /home/peter/minecraft.sh
