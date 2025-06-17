6e3efa3796f8812441954c843f7caf0c81f1269b
Peter Bergeron <Peter.Bergeron@gmail.com>
2025-06-17 00:54:41 -0400

    Update neovim script to hardlink the nvim binary so its available without needing to add it to the path

--- deploy/installers/neovim.sh
+++ deploy/installers/neovim.sh
@@ -3,3 +3,6 @@ curl -LO https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x
 sudo rm -rf /opt/nvim-linux-*
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 sudo rm nvim-linux-x86_64.tar.gz
+if [ ! -f /usr/bin/nvim ]; then
+ ln /opt/nvim-linux-x86_64/bin/nvim /usr/bin/nvim
+fi
